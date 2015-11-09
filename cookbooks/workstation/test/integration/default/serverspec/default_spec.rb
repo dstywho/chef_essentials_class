275f07ad8bd1c736927749fcf993da3766bd88c7
Dustin Tsang <dtsang@redhat.com>
2015-11-09 22:23:05 +0000

    update tests to use serverspec dsl

--- cookbooks/workstation/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/workstation/test/integration/default/serverspec/default_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 describe 'workstation::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'instaslls nano' do
-    `rpm -qa`.include?('nano')
-  end
-  it 'instaslls tree' do
-    `rpm -qa`.include?('tree')
+  describe package('tree') do
+    it { shoule be_installed }
   end
 end
